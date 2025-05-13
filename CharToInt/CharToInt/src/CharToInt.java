3e8048b93a10fd1cb6b7b30bcf6ee3e1df4d6b65
bet0dev <53548235+jr-george@users.noreply.github.com>
2025-05-13 16:42:04 -0600

    Add files via upload

---
+++ CharToInt/CharToInt/src/CharToInt.java
@@ -0,0 +1,7 @@
+public class CharToInt {
+    public static void main(String[] args) {
+        char a = 'a';
+        System.out.println("Valor de a: " + a);
+        System.out.println("Valor de a: " + (int)a);
+    }
+}
